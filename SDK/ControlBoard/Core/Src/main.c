23473f1ebd4978ba0462566c4b163254d2398b1b
sch <35249298@qq.com>
2023-12-21 23:53:54 +0800

    Problem:SWD low signal quality;Change: new layout

--- SDK/ControlBoard/Core/Src/main.c
+++ SDK/ControlBoard/Core/Src/main.c
@@ -23,6 +23,7 @@
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
 
+
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
