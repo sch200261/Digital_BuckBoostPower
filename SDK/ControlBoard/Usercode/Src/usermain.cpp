4ec1c51a822be2bd2a6c1605dfc3561e6ae6eb66
sch <35249298@qq.com>
2023-12-26 05:46:37 +0800

    delete LCD Lib for some technical problems

--- SDK/ControlBoard/Usercode/Src/usermain.cpp
+++ SDK/ControlBoard/Usercode/Src/usermain.cpp
@@ -8,15 +8,14 @@
 using namespace std;
 void StartDefaultTask(void const *argument)
 {
-    LedMain.LED_ON();
+
     PWMA.SetDuty(0.5000);
     PWMA.PWM_ON();
     PWMB.SetDuty(0.2000);
     PWMB.PWM_ON();
+
     while (true) {
-        LedMain.SetColor(ColorInit);
-        vTaskDelay(1000);
-        LedMain.SetColor(ColorWarning);
+
         vTaskDelay(1000);
     }
 
